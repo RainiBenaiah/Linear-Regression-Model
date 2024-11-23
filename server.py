24465a854090a58be6c57034528bb231662cd674
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2024-11-23 14:33:35 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,10 +3,5 @@ from main import app
 import os
 
 if _name_ == "_main_":
-    port = int(os.environ.get("RENDER_PORT", "8000"))
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=port,
-        log_level="info"
-    )
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
