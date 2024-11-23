eef73b39ab7efc279eaf78b8ce3f342e040fedb7
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2024-11-24 00:21:49 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,5 +3,7 @@ from main import app
 import os
 
 if __name__ == "__main__":
+    # Get the port from the environment variable or use 8000 as default
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    # Run the FastAPI application using uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
